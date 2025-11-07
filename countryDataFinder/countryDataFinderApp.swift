35038daf3e1151540cf0f24c4cd022888a68e0f8
esraa maged <esraa.magedali@gmail.com>
2025-11-07 13:27:05 +0200

    Initial Commit

---
+++ countryDataFinder/countryDataFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  countryDataFinderApp.swift
+//  countryDataFinder
+//
+//  Created by Esraa Eldaltony on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct countryDataFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ countryDataFinderTests/countryDataFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  countryDataFinderTests.swift
+//  countryDataFinderTests
+//
+//  Created by Esraa Eldaltony on 07/11/2025.
+//
+
+import Testing
+@testable import countryDataFinder
+
+struct countryDataFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
